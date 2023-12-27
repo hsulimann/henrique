90138e53b437e3858ef20546d0bc33d5d14022ac
Scaffolder <scaffolder@backstage.io>
2023-12-27 12:36:25 -0300

    initial commit

---
+++ app/henrique/src/main/java/com/hsulimann/henrique/henriqueApplication.java
@@ -0,0 +1,13 @@
+package com.hsulimann.henrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class henriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(henriqueApplication.class, args);
+	}
+
+}
